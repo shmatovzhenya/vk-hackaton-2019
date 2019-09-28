8b2ec1d8900a3cf2cd27b8e227871da7ff6f2f8b
shmatovzhenya <shmatovzhenya@yandex.ru>
2019-09-29 01:10:03 +0300

    Added finished orders to orders pages.

--- src/panels/Orders.js
+++ src/panels/Orders.js
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
+import refresh from '../img/refresh-button.svg';
 import './orders.css';
 
 
@@ -8,6 +9,40 @@ const Orders = ({ order, orderStatuses, foodAreas, onChangeOrderStatus, onRepeat
   const activeOrders = useMemo(() => {
     const activeOrdersList = Object.keys(orderStatuses)
       .filter(shopId => orderStatuses[shopId] === 'ACTIVE')
+      .map(shopId => shopId);
+
+    const result = [];
+    const activeOrdersSet = new Set(activeOrdersList);
+
+    foodAreas.forEach((area) => {
+      area.items.forEach(item => {
+        if (activeOrdersSet.has(item.id)) {
+          const data = {
+            placeName: area.name,
+            shopName: item.name,
+            price: item.foods.reduce((result, food) => {
+              if (food.id in order) {
+                const { item: { price }, count } = order[food.id];
+
+                return result + parseInt(price) * parseInt(count);
+              }
+
+              return result;
+            }, 0),
+            link: `/order/${area.id}/${item.id}`,
+          };
+
+          result.push(data);
+        }
+      });
+    });
+
+    return result;
+  }, [ order, orderStatuses ]);
+
+  const finishedOrders = useMemo(() => {
+    const activeOrdersList = Object.keys(orderStatuses)
+      .filter(shopId => orderStatuses[shopId] !== 'ACTIVE')
       .map(shopId => shopId);
 
     const result = [];
@@ -56,6 +91,26 @@ const Orders = ({ order, orderStatuses, foodAreas, onChangeOrderStatus, onRepeat
             <div>
               ~ 15 М
             </div>
+          </li>
+        ))}
+      </ul>
+      <ul className="Orders__finished-orders">
+        {finishedOrders.map(order => (
+          <li
+            className="Orders__finished-order"
+            key={order.link}
+          >
+            <div>
+              <h3>{order.placeName}</h3>
+              <p>{order.shopName}</p>
+              <p>Сумма {order.price} - Оплачено</p>
+            </div>
+            <button>
+              <img
+                alt="repeat order"
+                src={refresh}
+              />
+            </button>
           </li>
         ))}
       </ul>

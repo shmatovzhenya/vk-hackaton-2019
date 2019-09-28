c6b8299b1269466ca2d344e30c362731b6fb811d
shmatovzhenya <shmatovzhenya@yandex.ru>
2019-09-28 10:46:54 +0300

    Added header styles

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -2,30 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import './Home.css';
+
 
 const Home = ({ foodAreas }) => (
-	<ul>
-		{foodAreas.map((area) => (
-			<li
-				key={area.id}
-			>
-				<h2>{area.name}</h2>
-				<ul>
-					{area.items.map(item => (
-						<li 
-							key={item.id}
-						>
-							<Link
-								to={item.link}
-							>
-								<h3>{item.name}</h3>
-							</Link>
-						</li>
-					))}
-				</ul>
+	<React.Fragment>
+		<header className="Home__header">
+			<h1 className="Home__head">ТРЦ "Им.Пыжикова"</h1>
+			<Link to="/edit" className="Home__change-tz">
+				Ch
+			</Link>
+		</header>
+		<ul className="Home__tabs">
+			<li className="Home__tab Home__tab_active">
+				Еда
 			</li>
-		))}
-	</ul>
+			<li className="Home__tab Home__tab_disabled">
+				Развлечения
+			</li>
+			<li className="Home__tab Home__tab_disabled">
+				Здоровье
+			</li>
+		</ul>
+		<ul>
+			{foodAreas.map((area) => (
+				<li
+					key={area.id}
+				>
+					<h2>{area.name}</h2>
+					<ul>
+						{area.items.map(item => (
+							<li 
+								key={item.id}
+							>
+								<Link
+									to={item.link}
+								>
+									<h3>{item.name}</h3>
+								</Link>
+							</li>
+						))}
+					</ul>
+				</li>
+			))}
+		</ul>
+	</React.Fragment>
 );
 
 Home.propTypes = {
